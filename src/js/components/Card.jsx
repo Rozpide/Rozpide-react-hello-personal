00200f598039f75c962bd7abd07581adb097a862
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-12-17 00:20:31 +0000

    ajusto las card ventanas

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,88 +1,42 @@
-/*import React from "react";
-
-const Card = () => {
-    return (
-        <>
-        <div className="container-fluid">
-            <div className="card text-center mb-3" style={{ width: '18rem' }}>
-                    <div className="card-body">
-                        <img src="..." className="card-img-top" alt="..." />  
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div className="card text-center mb-3" style={{ width: '18rem' }}>
-                    <div className="card-body">
-                        <img src="..." className="card-img-top" alt="..." />  
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div className="card text-center mb-3" style={{ width: '18rem' }}>
-                    <div className="card-body">
-                        <img src="..." className="card-img-top" alt="..." />  
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-                <div className="card text-center mb-3" style={{ width: '18rem' }}>
-                    <div className="card-body">
-                        <img src="..." className="card-img-top" alt="..." />  
-                        <h5 className="card-title">Special title treatment</h5>
-                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-        </div>
-            
-        </>
-    );
-}
-
-export default Card;
-*/
 import React from "react";
 
 const Card = () => {
     return (
         <>
             <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-3">
-                        <div className="card text-center mb-3" style={{ width: '18rem' }}>
+                <div className="row justify-content-between">
+                    <div className="col-md-3 mb-3">
+                        <div className="card text-center" style={{ width: '18rem' }}>
                             <div className="card-body">
-                                <img src="..." className="card-img-top fluid" alt="..." />  
+                                <img src="..." className="card-img-top" alt="..." />  
                                 <h5 className="card-title">Special title treatment</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3">
-                        <div className="card text-center mb-3" style={{ width: '18rem' }}>
+                    <div className="col-md-3 mb-3">
+                        <div className="card text-center" style={{ width: '18rem' }}>
                             <div className="card-body">
-                                <img src="..." className="card-img-top fluid" alt="..." />  
+                                <img src="..." className="card-img-top" alt="..." />  
                                 <h5 className="card-title">Special title treatment</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3">
-                        <div className="card text-center mb-3" style={{ width: '18rem' }}>
+                    <div className="col-md-3 mb-3">
+                        <div className="card text-center" style={{ width: '18rem' }}>
                             <div className="card-body">
-                                <img src="..." className="card-img-top fluid" alt="..." />  
+                                <img src="..." className="card-img-top" alt="..." />  
                                 <h5 className="card-title">Special title treatment</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                                 <a href="#" className="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3">
-                        <div className="card text-center mb-3" style={{ width: '18rem' }}>
+                    <div className="col-md-3 mb-3">
+                        <div className="card text-center" style={{ width: '18rem' }}>
                             <div className="card-body">
                                 <img src="..." className="card-img-top" alt="..." />  
                                 <h5 className="card-title">Special title treatment</h5>
@@ -98,4 +52,5 @@ const Card = () => {
 }
 
 export default Card;
+
 
